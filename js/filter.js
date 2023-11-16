b1cf2b88b0a354f4c9554e161505a99fbba6975d
Tafannum-Ferdous <143213075+Tafannum-Ferdous@users.noreply.github.com>
2023-11-16 17:21:03 +0530

    Add files via upload

---
+++ js/filter.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
